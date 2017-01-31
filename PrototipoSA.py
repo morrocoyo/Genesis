cb08feaa7c24cde3975ee257c7e354c24ffa2de0
Morrocoyo <morrocoyofv@gmail.com>
2017-01-30 21:46:55 -0500

    Add files via upload

--- PrototipoSA.py
+++ PrototipoSA.py
@@ -21,7 +21,7 @@ from datetime import date,timedelta
 import unicodedata
 import nltk
 import re, string
-import CuentaTema
+#import CuentaTema
 from difflib import SequenceMatcher
 from CuentaTemaa import *
 """
@@ -30,19 +30,95 @@ el archivo Ambiente.txt
 """
 A=CuentaTemaa()
 """
-Carga las credenciales de acceso al api de twitter
+Se Cargan las credenciales de acceso al api de twitter
 """
-ACCESS_TOKEN = u'817383621397008384-TXP2pfAr0aLmr3E9Le3tDBlMSsUTQF9'
-ACCESS_SECRET = u'oBXXy2cmyZ80PUm6EhUHGAs7SRb13HxYO5TWj2fq9AYVN'
-CONSUMER_KEY = u'kpKOxDTCtWI8ueapP6JSyS9fM'
-CONSUMER_SECRET = u'ep7Di6fA1izMlQdQXBmcKiGT0q7PoXX2nDgaxFHjihA2of9aAu'
+ACCESS_TOKEN = u'817383621397008384-TXP2pfAr0aLmr3E9Le3tDBlMSsUTQFg'
+ACCESS_SECRET = u'oBXXy2cmyZ80PUm6EhUHGAs7SRb13HxYO5TWj2fqgAYVN'
+CONSUMER_KEY = u'kpKOxDTCtWI8ueapP6JSySgfM'
+CONSUMER_SECRET = u'ep7Di6fA1izMlQdQXBmcKiGT0q7PoXX2nDgaxFHjihA2ofgaAu'
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
 """
-Cico de Simulacion: Busqueda de tuits ambientales en Morrocoyo timeline + Streaming
+Ciclo de Simulacion: Busqueda de tuits ambientales en Morrocoyo TimeLine + Streaming
 """
+Influenciadores = pickle.load( open( 'Influenciadores', "rb" ) )
 LA=A.keys()
 twitter = Twitter(auth=oauth)
 MorroTL=twitter.statuses.home_timeline(count=200)
 k=1;
-for i in range(k,len(MorroTL)):
-     print i
+i=0;
+w, h = len(MorroTL), 3
+"""
+Crea un lista de 3 listas de tamaño Longitud de MorroTL con la calificacion de criterio para cada tweet 
+"""
+Criterios = [[0 for x in range(w)] for y in range(h)]
+c=0;
+L=dict();
+for tweet in MorroTL:
+    try:        #si es un retweet
+        try:        #si cita una pagina en el tweet
+            L[i]=[tweet['retweeted_status']['user']['name'],tweet['retweeted_status']['user']['screen_name'],
+              tweet['retweeted_status']['text'],tweet['retweeted_status']['created_at'],
+              tweet['retweeted_status']['retweet_count'],tweet['retweeted_status']['favorite_count'],
+              tweet['retweeted_status']['user']['description'],tweet['retweeted_status']['entities']['urls'][0]['url'],
+              tweet['created_at'],tweet['user']['time_zone'], tweet['user']['lang'], tweet['place'],
+              tweet['user']['verified'],tweet['user']['screen_name'],tweet['user']['description'],
+              tweet['user']['followers_count'],tweet['user']['friends_count'],tweet['user']['statuses_count']];
+        except:
+            L[i]=[tweet['retweeted_status']['user']['name'],tweet['retweeted_status']['user']['screen_name'],
+              tweet['retweeted_status']['text'],tweet['retweeted_status']['created_at'],
+              tweet['retweeted_status']['retweet_count'],tweet['retweeted_status']['favorite_count'],
+              tweet['retweeted_status']['user']['description'],'',
+              tweet['created_at'],tweet['user']['time_zone'], tweet['user']['lang'], tweet['place'],
+              tweet['user']['verified'],tweet['user']['screen_name'],tweet['user']['description'],
+              tweet['user']['followers_count'],tweet['user']['friends_count'],tweet['user']['statuses_count']];      
+    except:
+        try:        #si cita una pagina en el tweet
+            L[i]=[tweet['user']['name'],tweet['user']['screen_name'],
+              tweet['text'],tweet['created_at'],
+              tweet['retweet_count'],tweet['favorite_count'],
+              tweet['user']['description'],tweet['entities']['urls'][0]['url'],
+              tweet['created_at'],tweet['user']['time_zone'], tweet['user']['lang'], tweet['place'],
+              tweet['user']['verified'],'','',tweet['user']['followers_count'],
+              tweet['user']['friends_count'],tweet['user']['statuses_count']];
+        except:
+            L[i]=[tweet['user']['name'],tweet['user']['screen_name'],
+              tweet['text'],tweet['created_at'],
+              tweet['retweet_count'],tweet['favorite_count'],
+              tweet['user']['description'],'',
+              tweet['created_at'],tweet['user']['time_zone'], tweet['user']['lang'], tweet['place'],
+              tweet['user']['verified'],'','',tweet['user']['followers_count'],
+              tweet['user']['friends_count'],tweet['user']['statuses_count']]; 
+    try:
+        L[i].extend([tweet['entities']['media'][0]['media_url']]) 
+    except:
+        L[i].extend([''])
+    for r in LA:
+        try:
+            if r.lower() in L[i][2].encode('utf-8').lower().split():
+                c+=1
+        except:
+            pass
+    Criterios[0][i]=max(c,1)
+    c=0
+    try:
+        Criterios[1][i]=Influenciadores[L[i][1]]
+    except:
+        pass
+    try:
+        u=twitter.users.lookup(screen_name=L[i][1])  
+        if u[0]['followers_count']>60:
+            Criterios[2][i]=u[0]['followers_count']/u[0]['statuses_count']
+    except:
+        pass
+    print L[i][0]
+    print i
+    i+=1
+    
+    
+    
+    
+    
+    
+    
+    
+    
